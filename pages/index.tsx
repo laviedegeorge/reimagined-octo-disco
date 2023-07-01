9dfd4ae20f34e50c8ba701a060afd0f50f933c2f
Kelechi <laviedegeorge1292@gmail.com>
2023-07-01 20:55:26 +0100

    WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,212 +1,145 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
 import Head from "next/head";
+import classNames from "classnames";
+import { erc20ABI } from "./libs/contractABIs/erc20ABI";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import detectEthereumProvider from "@metamask/detect-provider";
+
+const erc20ContractAddress = "0x56a7Ee2292fA2483c8763fc093aBF6eBe64E13BB";
 
 /** eslint-ignore react/react-in-jsx-scope */
 export default function Home() {
+  const contractAddress = erc20ContractAddress;
+  const [loading, setLoading] = useState(false);
+  const [account, setAccount] = useState<null | string>(null);
+  const [contractInfo, setContractInfo] = useState({
+    tokenName: null,
+    tokenSymbol: null,
+    totalSupply: null,
+  });
+  const [contractInstance, setContractInstance] = useState<null | any>(null);
+
+  useEffect(() => {
+    const getProvider = async () => {
+      try {
+        setLoading(true);
+        const provider: MetaMaskInpageProvider = await detectEthereumProvider();
+        const accounts: string[] = await provider.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts[0]);
+
+        const web3 = new Web3(provider);
+        const contractInstance = new web3.eth.Contract(
+          erc20ABI,
+          contractAddress
+        );
+        setContractInstance(contractInstance);
+
+        const tokenName = await contractInstance?.methods?.name().call();
+        const totalSupply = await contractInstance?.methods
+          ?.totalSupply()
+          .call();
+        const tokenSymbol = await contractInstance?.methods?.symbol().call();
+        setContractInfo({ tokenName, tokenSymbol, totalSupply });
+
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.error(error);
+      }
+    };
+
+    getProvider();
+  }, [contractAddress]);
+
   return (
-    <div className="container">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title text-[#FFC0CB]">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid mx-auto border">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className=" min-w-full min-h-screen flex justify-center items-center relative">
+        <>
+          <div className="flex bg-slate-300 py-1 px-2 absolute top-0">
+            <div className="flex  items-center space-x-2">
+              <div
+                className={classNames("rounded-full w-4 h-4", {
+                  "bg-green-400": Boolean(account),
+                  "bg-red-500": Boolean(!account),
+                })}
+              ></div>
+              <p className="text-sm">
+                {account ? "Connected" : "Disconnected"}
+              </p>
+            </div>
+            <div className="w-[2px] bg-white mx-4"></div>
+            <p className="text-sm">{`${shortenAddress(account)}...`}</p>
+          </div>
+          <div>
+            {loading && <p>Loading ...</p>}
+            {!loading && account && contractInstance && (
+              <div>
+                <div>
+                  <p>
+                    <span className=" font-bold">TOKEN NAME:</span>{" "}
+                    {contractInfo?.tokenName}
+                  </p>
+                  <p>
+                    <span className=" font-bold">TOKEN SYMBOL:</span>{" "}
+                    {contractInfo?.tokenSymbol}
+                  </p>
+                  <p>
+                    <span className=" font-bold">TOTAL SUPPLY:</span>{" "}
+                    {`${contractInfo?.totalSupply} ${contractInfo?.tokenSymbol}`}
+                  </p>
+                </div>
+                <div className=" space-x-2">
+                  <button
+                    className="primary_btn"
+                    onClick={() =>
+                      getBalance(contractInstance, account, setLoading)
+                    }
+                  >
+                    {loading ? "Loading..." : "Get balance"}
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          margin: 0 auto;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <footer></footer>
     </div>
   );
 }
+
+const shortenAddress = (address) => {
+  try {
+    const addressArr = (address || "").split("");
+    const shortAddress = addressArr.splice(0, 6).join("");
+    return shortAddress;
+  } catch (error) {
+    console.log(error);
+    return "";
+  }
+};
+
+const getBalance = async (
+  contractInstance: any,
+  account: string,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  try {
+    setLoading(true);
+    const res = await contractInstance?.methods?.balanceOf(account).call();
+    alert(`Your balance is ${res} tBNB`);
+    setLoading(false);
+  } catch (error) {
+    console.error(error);
+    setLoading(false);
+  }
+};
